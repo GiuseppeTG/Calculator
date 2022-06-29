aaddbe5c98bd97275782c0f9fd902fd7ee93201f
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-06-29 11:30:01 -0500

    Refactor buttons with map

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -3,30 +3,36 @@
 
 import React from 'react';
 
+const buttonsRows = [
+  ['AC', '+/-', '%', '÷'],
+  ['7', '8', '9', 'x'],
+  ['4', '5', '6', '-'],
+  ['1', '2', '3', '+'],
+  ['0', '.', '='],
+];
 export default class Calculator extends React.Component {
   render() {
     return (
       <div className="calculator-grid">
         <div className="output">0</div>
-        <button className="calc-btn">AC</button>
-        <button className="calc-btn">+/-</button>
-        <button className="calc-btn">%</button>
-        <button className="calc-btn operator">÷</button>
-        <button className="calc-btn">7</button>
-        <button className="calc-btn">8</button>
-        <button className="calc-btn">9</button>
-        <button className="calc-btn operator">x</button>
-        <button className="calc-btn">4</button>
-        <button className="calc-btn">5</button>
-        <button className="calc-btn">6</button>
-        <button className="calc-btn operator">-</button>
-        <button className="calc-btn">1</button>
-        <button className="calc-btn">2</button>
-        <button className="calc-btn">3</button>
-        <button className="calc-btn operator">+</button>
-        <button className="span-two calc-btn">0</button>
-        <button className="calc-btn">.</button>
-        <button className="calc-btn operator">=</button>
+        {
+          buttonsRows.map((buttonsRow) => (
+            <>
+              {
+                  buttonsRow.map((buttonName, index) => (
+                    <button
+                      key={buttonName}
+                      className={`calc-btn 
+                      ${buttonName === '0' ? 'span-two' : ''} 
+                      ${index === buttonsRow.length - 1 ? 'operator' : ''}`}
+                    >
+                      { buttonName }
+                    </button>
+                  ))
+                }
+            </>
+          ))
+        }
       </div>
 
     );
