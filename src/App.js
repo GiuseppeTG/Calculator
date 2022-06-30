48a5e2e999b20f9f7629d062b4d4bb13168191e6
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-06-30 15:01:45 -0500

    functional component App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-// import Calculator from './components/calculator';
 import Calculator from './components/calculator';
 import './styles.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <Calculator />
-    );
-  }
-}
+const App = () => (
+  <Calculator />
+);
 
 export default App;
