fe0cfd4d3f7af64b0cf580a3a61f896513af57b6
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-06-30 14:53:40 -0500

    Functional component setup (Calculator2) clear

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-import Calculator from './components/calculator';
+// import Calculator from './components/calculator';
+import Calculator2 from './components/calculator2';
 import './styles.css';
 
 class App extends React.Component {
   render() {
     return (
-      <Calculator />
+      <Calculator2 />
     );
   }
 }
